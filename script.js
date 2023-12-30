a154e0b4b526dd6ee9767916a2498e96175ba856
ramaisha <ramaishaom@gmail.com>
2023-12-30 16:10:46 +0530

    recapcha adding

--- script.js
+++ script.js
@@ -1,8 +1,20 @@
-function beforesubmit(){
-let outputdate=document.querySelector(".outputdate");
-let inputdate=document.querySelector(".inputdate");
-console.log("inputdate.value",inputdate.value);//date --UserInfo.getLocale() --en_US
-let formatteddate=new Date(inputdate.value).toLocaleDateString("en-US");
-outputdate.value=formatteddate;
-
+function beforesubmit() {
+  let outputdate = document.querySelector(".outputdate");
+  let inputdate = document.querySelector(".inputdate");
+  console.log("inputdate.value", inputdate.value); //date --UserInfo.getLocale() --en_US
+  let formatteddate = new Date(inputdate.value).toLocaleDateString("en-US");
+  outputdate.value = formatteddate;
 }
+
+function timestamp() {
+  var response = document.getElementById("g-recaptcha-response");
+  if (response == null || response.value.trim() == "") {
+    var elems = JSON.parse(
+      document.getElementsByName("captcha_settings")[0].value
+    );
+    elems["ts"] = JSON.stringify(new Date().getTime());
+    document.getElementsByName("captcha_settings")[0].value =
+      JSON.stringify(elems);
+  }
+}
+setInterval(timestamp, 500);
