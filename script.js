358c07ed368863904257f478ecdcfb367e6678c6
ramaisha <ramaishaom@gmail.com>
2024-02-15 14:31:08 +0530

    script comment

--- script.js
+++ script.js
@@ -3,8 +3,9 @@ function beforesubmit(event) {
   if (captchachecked) {
     let outputdate = document.querySelector(".outputdate");
     let inputdate = document.querySelector(".inputdate");
-    console.log("inputdate.value", inputdate.value); //date --UserInfo.getLocale() --en_US
-    let formatteddate = new Date(inputdate.value).toLocaleDateString("en-US");
+    console.log("inputdate.value", inputdate.value); //string ---> date(en-US) (lead owner locale)
+    console.log("ramesh");
+    let formatteddate = new Date(inputdate.value).toLocaleDateString("en-US"); //converting date to "en-US" (converting to lead owner locale)
     outputdate.value = formatteddate;
   } else {
     alert("please check the reCAPTCHA box to submit the lead");
